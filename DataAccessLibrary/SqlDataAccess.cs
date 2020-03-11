97e24c7765967c615d1d2da24f355a8f05a2bd4b
Chris Barcia <chrisbarcia@outlook.com>
2020-03-11 00:16:04 -0400

    IConfiguration constructor

---
+++ DataAccessLibrary/SqlDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary
+{
+    public class SqlDataAccess
+    {
+        private readonly IConfiguration config;
+
+        public string ConnectionStringName { get; set; } = "Default";
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            this.config = config;
+        }
+    }
+}
